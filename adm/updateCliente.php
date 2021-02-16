0ab802573d9a5e9f525e4301529fb354ed034c06
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2021-02-16 13:18:07 -0300

    Validando cpf ao editar

--- adm/updateCliente.php
+++ adm/updateCliente.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
 include '../conecta.php';
+include 'funcoes.php';
+
 include '../mensagemPadrao.php';
 $idCliente = $_GET["id"];
 $situacao = $_POST['ativo'];
@@ -10,16 +12,32 @@ $email = $_POST['email'];
 $telefone = $_POST["telefone"];
 $endereco = $_POST["endereco"];
 $atacado = $_POST["atacado"];
-$sqlUpdate = "UPDATE Cliente SET nomeCliente = '$nomeCliente', atacado = '$atacado' , cpf_cnpj = '$cpf',
-            emailCliente = '$email', telefoneCliente = '$telefone', ativo = '$situacao',enderecoCliente = '$endereco'  
-          where idCliente=$idCliente";
-
-if ($conn->query($sqlUpdate) === TRUE) {
-    $_SESSION['msg'] = $mensagens["edicao"];
-
+$pesquisaUsuarios = "SELECT cpf_cnpj from cliente u  where u.cpf_cnpj= $cpf";
+$Usuarios = mysqli_query($conn, $pesquisaUsuarios);
+if (!validaCPF($cpf)) {
+    $_SESSION['msg'] = $mensagens["cpfInvalido"];
     header("Location:usuario.php");
 } else {
 
-    $_SESSION['msg'] = $mensagens["erroEdicao"];
-    header("Location:usuario.php");
+    //Contar o total de logs
+    $totalUsuarios = mysqli_num_rows($Usuarios);
+    if ($totalUsuarios > 0) {
+        $_SESSION['msg'] = $mensagens["cpfDuplicado"];
+        header("Location:usuario.php");
+    } else {
+
+        $sqlUpdate = "UPDATE Cliente SET nomeCliente = '$nomeCliente', atacado = '$atacado' , cpf_cnpj = '$cpf',
+            emailCliente = '$email', telefoneCliente = '$telefone', ativo = '$situacao',enderecoCliente = '$endereco'  
+          where idCliente=$idCliente";
+
+        if ($conn->query($sqlUpdate) === TRUE) {
+            $_SESSION['msg'] = $mensagens["edicao"];
+
+            header("Location:usuario.php");
+        } else {
+
+            $_SESSION['msg'] = $mensagens["erroEdicao"];
+            header("Location:usuario.php");
+        }
+    }
 }
