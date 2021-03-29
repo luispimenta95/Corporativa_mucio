8e61f35c6dcd9a1be91df74e5004a86b708cf32c
Luis Pimenta <luisfelipearaujopimenta@gmail.com>
2021-03-29 19:41:52 -0300

    Dados banco

--- conecta.php
+++ conecta.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new MySQLi('localhost', 'u774307099_root', 'Mp13151319', 'u774307099_legrano');
+$conn = new MySQLi('localhost', 'u774307099_pimenta', 'Mp13151319', 'u774307099_legrano');
 if ($conn->connect_error) {
    echo "Desconectado! Erro: " . $conn->connect_error;
 }
